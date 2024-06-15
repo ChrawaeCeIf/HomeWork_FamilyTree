e8f2ede2ccf23fb7760065f45e8c977375f6aa3e
Artem Glushkov <artyom.glushkov.2000@bk.ru>
2024-06-15 03:38:41 +0300

    Completed HomeWork

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import View.ConsoleUI;
+import View.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.start();
+    }
+}
